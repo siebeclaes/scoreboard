dd9572d54c344fa6015883b82e67893d421b9776
Siebe Claes <siebe.claes@gmail.com>
2019-11-11 22:12:09 +0100

    Updates

---
+++ precache-manifest.8c43936dff7054b298476d3f6400f0b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e9d0457f75ac6441805d712f727a49",
+    "url": "/scoreboard/index.html"
+  },
+  {
+    "revision": "7bf006b756ed13647e1a",
+    "url": "/scoreboard/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "539bae6fae64e370e92c",
+    "url": "/scoreboard/static/css/main.0b224aaa.chunk.css"
+  },
+  {
+    "revision": "7bf006b756ed13647e1a",
+    "url": "/scoreboard/static/js/2.b0fb85d7.chunk.js"
+  },
+  {
+    "revision": "539bae6fae64e370e92c",
+    "url": "/scoreboard/static/js/main.eeb26b9b.chunk.js"
+  },
+  {
+    "revision": "380ec52b7ec6a818b29c",
+    "url": "/scoreboard/static/js/runtime-main.12cae5e1.js"
+  }
+]);
